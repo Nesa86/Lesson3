4734ef706743c165af7711a5e7e24ca13a2d0775
unknown <nenaddikic86@gmail.comgit config --global user.name Nesa86>
2019-05-15 19:00:23 +0200

    Update

--- src/optional/RobotColorChooser.java
+++ src/optional/RobotColorChooser.java
@@ -12,11 +12,38 @@ public class RobotColorChooser {
 
 		// 5. if the user doesn’t enter anything, choose a random color
 
-		// 6. put a loop around your code so that you keep asking the user for more colors & drawing them
+		// 6. put a loop around your code so that you keep asking the user for more
+		// colors & drawing them
 
 		// 2. set the pen width to 10
 
 		// 1. make the robot draw a shape (this will take more than one line of code)
+
+		Robot robby = new Robot();
+
+		for (int i = 0; i < 10; i++) {
+
+			String ans = JOptionPane.showInputDialog("Type what color would you like the tortoise to draw:");
+
+			if (ans.equals("red")) {
+				robby.setPenColor(java.awt.Color.red);
+			} else if (ans.equals("blue")) {
+				robby.setPenColor(java.awt.Color.blue);
+			} else if (ans.equals("yellow")) {
+				robby.setPenColor(java.awt.Color.yellow);
+			} else {
+				robby.setRandomPenColor();
+			}
+			robby.setPenWidth(10);
+			robby.setSpeed(10);
+			robby.penDown();
+			for (int j = 0; j < 4; j++) {
+				robby.move(100);
+				robby.turn(90);
+
+			}
+
+		}
 
 	}
 }
