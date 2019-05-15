8a889b22b63a02600ec7ddff5b45499458576355
unknown <nenaddikic86@gmail.comgit config --global user.name Nesa86>
2019-05-15 18:59:11 +0200

    Update

--- src/exercises/HighLowGame.java
+++ src/exercises/HighLowGame.java
@@ -1,6 +1,40 @@
 package exercises;
 
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+import java.util.Random;
+
 public class HighLowGame {
 
+	public static void main(String[] args) {
 
-}
+		
+
+		Random rand = new Random();
+		
+		int correctNum = rand.nextInt(10);
+
+	
+
+		while (true) {
+			Scanner sc = new Scanner(JOptionPane.showInputDialog("Enter a number from 1 to 10"));
+		    int guess1 = sc.nextInt();
+
+		    if(guess1 < correctNum){
+		    	JOptionPane.showMessageDialog(null, "Number is too low!");
+		    }
+		    else if(guess1 > correctNum){
+		    	JOptionPane.showMessageDialog(null, "Number is too high!");
+		    }
+		    else if(guess1 == correctNum){
+		    	JOptionPane.showMessageDialog(null, "Correct!");
+		        break; 
+		        
+		    }
+		    else{
+		    	JOptionPane.showMessageDialog(null, "Not valid");
+		    }
+		}
+}}
