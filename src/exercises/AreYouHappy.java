a457d34efba3818f291eafa9320a0d55a5713723
unknown <nenaddikic86@gmail.comgit config --global user.name Nesa86>
2019-05-15 18:53:05 +0200

    Update

--- src/exercises/AreYouHappy.java
+++ src/exercises/AreYouHappy.java
@@ -1,7 +1,33 @@
 package exercises;
 
+import javax.swing.JOptionPane;
+
 /* See the diagram in the "doc" directory. */
 
 public class AreYouHappy {
+	public static void main(String[] args) {
 
+		String answer = JOptionPane.showInputDialog("Are you happy?");
+		if (answer.contentEquals("yes")) {
+
+			JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing!");
+		}
+
+		else if (answer.contentEquals("no")) {
+			String answer2 = JOptionPane.showInputDialog("Do you want to be happy?");
+			{
+				if (answer2.contentEquals("yes")) {
+
+					JOptionPane.showMessageDialog(null, "Change something");
+				} else
+
+				{
+					JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing!");
+				}
+
+			}
+		} else {
+			JOptionPane.showMessageDialog(null, "Unknown entry");
+		}
+	}
 }
