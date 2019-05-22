92bd6c973e19c8b853b7abcf4d760fc1946e7555
unknown <nenaddikic86@gmail.comgit config --global user.name Nesa86>
2019-05-22 21:54:05 +0200

    Update

--- src/optional/CountQuarters.java
+++ src/optional/CountQuarters.java
@@ -10,21 +10,20 @@ public class CountQuarters extends TestCase {
 	public void test() throws Exception {
 		assertEquals(0, countQuarters(0.25));
 		assertEquals(0, countQuarters(1));
-<<<<<<< HEAD
+
 		assertEquals(1, countQuarters(25.26));
-=======
+
 		assertEquals(14, countQuarters(0.64));
->>>>>>> ad8adb218dabdc1af763a84d5f9cf3e65a6a3676
+
 		assertEquals(3, countQuarters(12.78));
 	}
 
 	private Object countQuarters(double money) {
-<<<<<<< HEAD
+
 		return (int) (money * 100) % 25;
-=======
-		// TODO Auto-generated method stub
-		return null;
->>>>>>> ad8adb218dabdc1af763a84d5f9cf3e65a6a3676
+
+		
+
 	}
 
 }
